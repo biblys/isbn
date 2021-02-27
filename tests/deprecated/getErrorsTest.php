dd08d43e6026d456bc28aaf773845b617a93aa70
Clément Bourgoin <c@iwzr.fr>
2021-02-27 21:49:31 +0100

    Move deprecated code test into tests/deprecated directory

--- tests/deprecated/getErrorsTest.php
+++ tests/deprecated/getErrorsTest.php
@@ -14,7 +14,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Biblys\Isbn\Isbn as Isbn;
 use PHPUnit\Framework\TestCase;
