8b7fcb5612e69c5a58f15c0ab8821d24211c03b5
Daniel Jagszent <daniel@jagszent.de>
2022-03-08 14:07:39 +0100

    refactor: pre-process data arrays
    Pre-process the XML file into a more compact and better parsable data structure. It uses associative arrays for prefix lookups and pre-splits the ranges.

---
+++ src/Biblys/Isbn/prefixes-array.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+ * This file is generated automatically by update-ranges.php, do not edit
+ * manually! See README.md for more info. 
+ *
+ * This file is part of the biblys/isbn package.
+ *
+ * (c) Clément Bourgoin
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+return [
+    '978' => ['International ISBN Agency', [
+        ['0000000', '5999999', 1],
+        ['6000000', '6499999', 3],
+        ['6500000', '6599999', 2],
+        ['6600000', '6999999', 0],
+        ['7000000', '7999999', 1],
+        ['8000000', '9499999', 2],
+        ['9500000', '9899999', 3],
+        ['9900000', '9989999', 4],
+        ['9990000', '9999999', 5],
+    ]],
+    '979' => ['International ISBN Agency', [
+        ['0000000', '0999999', 0],
+        ['1000000', '1299999', 2],
+        ['1300000', '7999999', 0],
+        ['8000000', '8999999', 1],
+        ['9000000', '9999999', 0],
+    ]],
+];
